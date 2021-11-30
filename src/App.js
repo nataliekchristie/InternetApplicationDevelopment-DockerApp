c1eff78a392fee832f29f36116bd76d20d31d1a8
Natalie Christie <nchristie@mail.sfsu.edu>
2021-11-30 12:16:08 -0800

    Updated App.js
    - added creation listing form
    - added functions for retrieving listings
    - created listing map that loads listings upon page load

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 import { useSelector, useDispatch } from 'react-redux';
 import { updateMessages, handlTextChange, submitMessage } from './redux/actions/messageActions';
@@ -11,6 +11,52 @@ const App = ({ adminMode = true }) => {
   const messages = useSelector(state => state.messageReducer.messages);
   const dispatch = useDispatch();
 
+  // used for setting info when making new listing
+  const [title, setTitle] = useState();
+  const [description, setDescription] = useState();
+  const [type, setType] = useState();
+  const [price, setPrice] = useState();
+  
+  // for making messages
+  const [message, setMessage] = useState();
+
+  // used to stream existing listings
+  const [listings, setListings] = useState([]);
+  const addListing = newListing=> setListings(state => [...state, newListing]);
+
+  // use for when we create listing form 
+  function submitListing(e){
+    e.preventDefault();
+    axios.post('/api/makingListing', {
+      title: title,
+      description: description,
+      type: type,
+      price: price,
+    })
+    .then( res => {
+      console.log("Listing made")
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+  
+  // submit message saved in state
+/*
+  function submitMessage{
+
+  }*/
+
+  // uses api function to get listings from mongodb and stores in array
+  function getListings(){
+    axios.get('/getListings', (req,res) => {
+    })
+    .then((res) => res.data.forEach(listing => addListing(listing)))
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+
   React.useEffect(() => {
     axios.get('/messanger/getMessages')
       .then((res) => {
@@ -19,6 +65,8 @@ const App = ({ adminMode = true }) => {
       .catch((e) => {
         console.log(e);
       });
+      // call listings upon load
+      getListings();
   }, []);
 
   const onSubmit = () => {
@@ -47,27 +95,47 @@ const App = ({ adminMode = true }) => {
   if(adminMode){
   return(
     <div>
+      <div className ="listingCreationForm">
+      <form>
+        <textarea id="input-description" onChange={e => setDescription(e.target.value)}></textarea>
+        <textarea id="input-type" onChange={e => setType(e.target.value)}></textarea>
+        <textarea id="input-price" onChange={e => setPrice(e.target.value)}></textarea>
+        <textarea id="input-title" onChange={e => setTitle(e.target.value)}></textarea>
+        <button id="submit" onClick={() => submitListing()}>Submit</button>
+      </form>
+    </div>
+      <div className="listings">
+        {listings.map(listing => (
+        <div className="listing">
         <div className="listingTitle">{listing.title}</div>
         <div className="listingDescription">{listing.description}</div>
         <div className="listingType">{listing.type}</div>
         <div className="listingPrice">{listing.price}</div>
         <form>
-          <textarea name="textarea" placeholder="test" onChange={e => setMessage(e.target.value)} />
+          <textarea name="textarea" onChange={e => setMessage(e.target.value)} />
           {message}
           <button className="submit" onClick={() => submitInquiry()}></button>
         </form>
+        </div>
+        ))}
+      </div>
       </div>
   );
   }
   else{
     return(
       <div>
+        <div className="listings">
+        {listings.map(listing => (
+      <div className="listing">
       <div className="listingTitle">{listing.title}</div>
       <div className="listingDescription">{listing.description}</div>
       <div className="listingType">{listing.type}</div>
       <div className="listingPrice">{listing.price}</div>
       <div className="listingDelete" onClick={() => deleteListing()}>Delete</div>
-      <div className="listingInquiries" onClick={() => viewInquiries()}>View Inquiries</div>
+      </div>
+        ))}
+      </div>
     </div>
 
     );
